c9a845e9232086958dc66108e65e175b87aa9cfb
Cyclohexanol <ambroise.mean@gmail.com>
2023-03-19 19:33:56 +0100

    Persistant storage

--- api/config.py
+++ api/config.py
@@ -1,17 +1,11 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 import os
 from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-
 class BaseConfig():
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'apidata.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('/app/db', 'apidata.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "saamblove-2023"
     JWT_SECRET_KEY = "saamblove-2023"
